9e8e3f79b29a579a8fb7b7ba060cc482f963f98f
David Martinez <dvdmarz1@gmail.com>
2020-09-10 10:07:07 -0500

    fin seccion 3 - diseño

---
+++ src/app/shared/breadcrunmbs/breadcrunmbs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrunmbs',
+  templateUrl: './breadcrunmbs.component.html',
+  styles: [
+  ]
+})
+export class BreadcrunmbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
